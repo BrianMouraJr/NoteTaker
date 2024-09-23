80304f08cdd2d440a7750cc614d7d8568eb7070f
Brian Moura <brian.prorealestate@gmail.com>
2024-09-23 15:07:51 -0400

    updating readme, working code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NoteTaker
+
+An app to jot down notes and organize them accordingly. 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 const htmlRoutes = require('./routes/htmlroutes')
 const apiRoutes = require('./routes/apiroutes')
 
-// top to bottom, html routes on bottom else catchall blocks /api,apiroutes 
+// top to bottom, html routes on bottom else catchall line blocks /api,apiroutes 
 app.use(express.json());
 app.use(express.urlencoded({extended:true})) //encode special characters so that its readable to the server
 app.use(express.static("public")) //server telling web browser what folder to treat as root, look for index.html here
